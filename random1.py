45b79ca70c0f2a73e03dc102658103b3801031bd
zhangwenchao <860694896@qq.com>
2021-09-29 15:16:12 +0800

    字符串随机

---
+++ random1.py
@@ -0,0 +1,14 @@
+import random
+
+names = ['段月楼',
+         '张文超',
+         '陆青运',
+         '王佳林',
+         '任先平',
+         '胡立川',
+         '徐高林',
+         '孙卓',
+         '李云峰',
+         '王文风']
+random.shuffle(names)
+print(names)
